21c8ba8eb97b143c1948623f2bfb5e8e48aa91ba
Luka Vincelj <Luka Vincelj@WIN-C9396DGII6R>
2026-03-29 12:39:52 +0200

    Add project files.

---
+++ HospitalManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HospitalManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
